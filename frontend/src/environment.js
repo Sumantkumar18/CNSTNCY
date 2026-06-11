26614b187de2cb1d3093d0e95990e4643f90b8ce
Sumantinvisible18 <sumantme2020@gmail.com>
2026-06-11 15:19:09 +0530

    update backend URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,5 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
+    "https://cnstncweb-bh9p.onrender.com" :
     "http://localhost:8000"
-
-
 export default server;
