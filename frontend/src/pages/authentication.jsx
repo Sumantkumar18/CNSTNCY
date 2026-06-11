9e6fd3d28541a173b5fab37f183ed7037e6a8b80
Sumantinvisible18 <sumantme2020@gmail.com>
2026-06-11 16:07:42 +0530

    fix background image

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -76,7 +76,7 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundImage: 'url(https://images.unsplash.com/photo-1616531770192-6eaea74c2456?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
